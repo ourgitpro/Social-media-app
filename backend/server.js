974383017d01045f1a9eef0114bdbb0e79dcec16
Md. Nahid Hossain Eblo <56239953+ourgitpro@users.noreply.github.com>
2022-08-24 10:25:53 -0700

    yes
    hi

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const app = express()
-app.use(cors())
-dotenv.config()
-app.get('/', function (req, res) {
-  res.send('Hello World hi')
-})
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { readdirSync } = require("fs");
+const app = express();
 
-app.listen(8000,()=>{
-    console.log('running port 3000')
-})
+dotenv.config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+readdirSync("./routes").map((f) => app.use("/", require("./routes/" + f)));
+
+// database
+mongoose.connect(process.env.MONGODB_CONNECTION_URL).then(() => {
+  console.log("Database Connected");
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
+//
+//mongoose.connect('mongodb+srv://user:user123456@cluster0.ld9e4.mongodb.net/WWE?retryWrites=true&w=majority',()=>{
+ // console.log("DB Connected")
